2abb3c5458c25d98e574e54c1f7d3acbdc2114ba
parul <parul.rawat@comprotechnologies.com>
2019-10-14 16:13:16 +0530

    cleaning

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const registered_tool = require('../registered_data/tool.json')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'LTI 1.3 Platform' });
 });
 
